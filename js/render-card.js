2be05687493a5451dad30815cb6dd5315e17168d
Helen <helenlena52@gmail.com>
2023-01-12 19:44:51 +0200

    add-render-by-default-and-card-style

--- js/render-card.js
+++ js/render-card.js
@@ -11,16 +11,29 @@ function getValue(e) {
   const value = e.target[0].value;
 
   fetchWeather(value)
-    .then((data) => renderCard(data))
+    .then((data) => {
+      console.log(data);
+      renderCard(data);
+    })
     .catch((error) => console.log(error));
+
+  e.target[0].value = "";
 }
 
 function renderCard(data) {
   const temp = Ktransform(data.main.temp);
 
   const markup = `
-    <h2>${data.name}</h2>
-    <div><span>${temp}°С</span></div>`;
+    <div class='root__card'>
+      <h2 class='root__card-title'>${data.name}</h2>
+      <div class='root__card-about'>
+        <img class='root__card-img' src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+        <span class='root__card-temp'>${temp}°С</span>
+        <img class='root__card-img' src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+      </div>
+      <p class='root__card-description'>${data.weather[0].main}</p>
+    </div>
+    `;
 
   cardRootRef.innerHTML = markup;
 }
@@ -28,3 +41,10 @@ function renderCard(data) {
 const Ktransform = (kelvin) => {
   return Math.floor(kelvin - 273.15);
 };
+
+fetchWeather("miami")
+  .then((data) => {
+    console.log(data);
+    renderCard(data);
+  })
+  .catch((error) => console.log(error));
