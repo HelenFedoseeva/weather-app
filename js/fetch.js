f7b07bfc3be6bf42f90477e4513ac93490ed6415
Helen <helenlena52@gmail.com>
2023-01-12 16:00:20 +0200

    add-render-card

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,16 @@
-// const axios = require("axios").default;
-
 const apiKey = "8af6b99c11d353b6ab61272f0ea7eae0";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather?";
 
-async function fetch(city, apiKey) {
-  return await axios
-    .get(`${URL}q=${city}&appid=${apiKey}`)
-    .then((response) => response.data);
+function fetchWeather(city) {
+  return fetch(`${URL}q=${city}&appid=${apiKey}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch((error) => console.log(error));
 }
 
-export { fetch };
+export { fetchWeather };
--- js/render-card.js
+++ js/render-card.js
@@ -1,4 +1,30 @@
-import { fetch } from "./fetch.js";
+import { fetchWeather } from "./fetch.js";
 
 const cardRootRef = document.getElementById("root");
-console.log(cardRootRef);
+const valueRef = document.querySelector(".main__label-input");
+const formRef = document.querySelector(".main-form");
+
+formRef.addEventListener("submit", getValue);
+
+function getValue(e) {
+  e.preventDefault();
+  const value = e.target[0].value;
+
+  fetchWeather(value)
+    .then((data) => renderCard(data))
+    .catch((error) => console.log(error));
+}
+
+function renderCard(data) {
+  const temp = Ktransform(data.main.temp);
+
+  const markup = `
+    <h2>${data.name}</h2>
+    <div><span>${temp}°С</span></div>`;
+
+  cardRootRef.innerHTML = markup;
+}
+
+const Ktransform = (kelvin) => {
+  return Math.floor(kelvin - 273.15);
+};
