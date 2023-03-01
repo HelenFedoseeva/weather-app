2d554c5ca36467cdbbecfb6f1f662dda8c404bfd
Helen <helenlena52@gmail.com>
2023-03-01 09:34:38 +0200

    add mobile-menu transfrom

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,12 +1,7 @@
-import { closeMobMenuClickOutside } from "./mobile-menu.js";
-
 const burgerMenuRef = document.querySelector(".header__menu-burger");
 
 burgerMenuRef.addEventListener("click", onBurgerClickHandler);
 
 function onBurgerClickHandler(e) {
-  e.preventDefault();
-  console.log(569);
-  document.getElementById("mobile-menu").classList.remove("hidden");
-  //   document.addEventListener("click", closeMobMenuClickOutside);
+  document.getElementById("mobile-menu").classList.toggle("transform");
 }
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,23 +4,27 @@ closeMobileMenuRef.addEventListener("click", onCloseMobileMenuHandler);
 document.addEventListener("keydown", closeMobileMenuEscHandler);
 
 function onCloseMobileMenuHandler(e) {
-  document.getElementById("mobile-menu").classList.toggle("hidden");
+  document.getElementById("mobile-menu").classList.remove("transform");
   document.removeEventListener("keydown", closeMobileMenuEscHandler);
-  document.removeEventListener("click", closeMobMenuClickOutside);
 }
 
 function closeMobileMenuEscHandler(e) {
   if (e.key === "Escape") {
-    document.getElementById("mobile-menu").classList.add("hidden");
-    // document.removeEventListener("click", closeMobMenuClickOutside);
+    document.getElementById("mobile-menu").classList.remove("transform");
   }
 }
 
-export function closeMobMenuClickOutside(e) {
-  console.log(e.target.classList.value);
+// export function closeMobMenuClickOutside(e) {
+//   console.log(e.target.classList.value);
 
-  if (e.target.classList.value !== "mobile-menu") {
-    document.getElementById("mobile-menu").classList.add("hidden");
-    document.removeEventListener("keydown", closeMobileMenuEscHandler);
-  }
-}
+//   if (
+//     e.target.classList.value === "mobile-menu" ||
+//     e.target.classList.value === "mobile-menu__item" ||
+//     e.target.classList.value === "mobile-menu__container"
+//   ) {
+
+//     return;
+//   }
+//   document.getElementById("mobile-menu").classList.add("hidden");
+//   document.removeEventListener("keydown", closeMobileMenuEscHandler);
+// }
